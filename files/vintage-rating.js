c6abbbb31445e0651272f9a398c8349d6929d689
Lütfi Şahindoğan <lutfi.sahindogan@icloud.com>
2020-09-27 16:28:12 +0300

    style changed

--- files/vintage-rating.js
+++ files/vintage-rating.js
@@ -26,8 +26,8 @@ function getBigRating(score){
         components.push(`
           <div class="rating-icon-container">
             <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#E5DED0"></path>
-                <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#E5DED0" stroke-width="2"></path>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#e5ecfd"></path>
+                <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#e5ecfd" stroke-width="2"></path>
             </svg>
          </div>`
         );
@@ -36,7 +36,7 @@ function getBigRating(score){
           <div class="rating-icon-container">
             <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#fff"></path>
-              <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#E5DED0" stroke-width="2"></path>
+              <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#e5ecfd" stroke-width="2"></path>
             </svg>
           </div>`
         );
@@ -55,8 +55,8 @@ function getSmallRating(score){
       components.push(`
         <div class="rating-icon-container">
           <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#E5DED0"></path>
-              <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#E5DED0" stroke-width="2"></path>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#e5ecfd"></path>
+              <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#e5ecfd" stroke-width="2"></path>
           </svg>
        </div>`
       );
@@ -65,7 +65,7 @@ function getSmallRating(score){
         <div class="rating-icon-container">
           <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M11.498 1.27a2 2 0 0 1 2.004 0l9.5 5.497A2 2 0 0 1 24 8.498v11.004a2 2 0 0 1-.998 1.73l-9.5 5.498a2 2 0 0 1-2.004 0l-9.5-5.497A2 2 0 0 1 1 19.502V8.498a2 2 0 0 1 .998-1.73l9.5-5.498z" fill="#fff"></path>
-            <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#E5DED0" stroke-width="2"></path>
+            <path d="M1 8.498v11.004a2 2 0 0 0 .998 1.73l9.5 5.498a2 2 0 0 0 2.004 0l9.5-5.497A2 2 0 0 0 24 19.502V8.498a2 2 0 0 0-.998-1.73l-9.5-5.498a2 2 0 0 0-2.004 0l-9.5 5.497A2 2 0 0 0 1 8.498z" stroke="#e5ecfd" stroke-width="2"></path>
           </svg>
         </div>`
       );
